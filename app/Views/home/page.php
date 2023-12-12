aa7b1a6dab1e2a77b78c23e1e83f94e5b639bcbf
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2023-12-12 15:45:01 +0700

    feat: layout template

---
+++ app/Views/home/page.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      Ini Contoh Halaman Home
+    </div>
+  </div>
+</div>
+<?= $this->endSection(); ?>
