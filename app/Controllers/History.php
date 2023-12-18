de98ef4ece9dd03c3ebc3d1e5c222180ec725b24
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2023-12-18 23:39:57 +0700

    feat: status

--- app/Controllers/History.php
+++ app/Controllers/History.php
@@ -16,11 +16,40 @@ class History extends BaseController
     $this->produkModel = new ProdukModel();
   }
 
-  public function index(): string
+  public function index()
   {
+    $apiUrl = 'http://localhost:8081/api/pengiriman';
+
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Waktu maksimal 10 detik
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+
+    if ($response) {
+      $pengirimanList = json_decode($response, true)['data'];
+    } else {
+      $pengirimanList = [];
+    }
+
+    $pesanan = $this->pesananModel->findAll();
+    // Membuat array asosiatif dari array2 berdasarkan id
+    $array2Assoc = [];
+    foreach ($pengirimanList as $item2) {
+      $array2Assoc[$item2['id_pesanan']] = $item2['status'];
+    }
+
+    // Menggunakan array_map untuk menyatukan array1 dengan array2
+    $joinedArray = array_map(function ($item1) use ($array2Assoc) {
+      $id = $item1['id_pesanan'];
+      $status = isset($array2Assoc[$id]) ? $array2Assoc[$id] : 'disiapkan';
+      return array_merge($item1, ['status' => $status]);
+    }, $pesanan);
+    // dd($joinedArray, $pengirimanList, $pesanan, $array2Assoc);
     $data = [
       'title' => 'Daftar Produk',
-      'orderHistory' => $this->pesananModel->findAll(),
+      'orderHistory' => $joinedArray,
       'produk' => $this->produkModel->findAll(),
       'notifikasi' => $this->notifikasiModel->getNotifikasiByUserId(session()->get('user_id'))
     ];
--- app/Views/orderHistory/indeks.php
+++ app/Views/orderHistory/indeks.php
@@ -2,58 +2,58 @@
 
 <?= $this->section('content'); ?>
 <div class="container py-4">
-    <div class="row justify-content-center align-items-center mb-5">
-        <div class="col-lg-9">
-            <h2 class="fw-bold mb-3">Order History</h2>
-            <?php if (session()->getFlashdata('pesan')) : ?>
-                <div class="alert alert-success my-2" role="alert">
-                    <?= session()->getFlashdata('pesan'); ?>
-                </div>
-            <?php endif; ?>
-            <div class="d-flex justify-content-center align-items-center w-100 mt-2 flex-column space-y-4">
-                <?php foreach ($orderHistory as $o) : ?>
-                    <?php
-                    $produk_id = $o["produk_id"];
-                    $matchingProduct = array_filter($produk, function ($produk) use ($produk_id) {
-                        return $produk['id'] == $produk_id;
-                    });
-                    $matchingProduct = reset($matchingProduct);
-                    ?>
-                    <div class="d-flex flex-row justify-content-start align-items-start border border-gray-200 w-100 px-3 my-3 py-3">
-                        <img src="/img/<?= $matchingProduct['gambar']; ?>" alt="<?= $matchingProduct['nama']; ?>" style="width: 20%; aspect-ratio: 1/1; object-fit: cover; object-position: center;">
-
-                        <div class="d-flex flex-column justify-content-start align-items-start w-100 px-4">
-                            <div class="d-flex justify-content-between align-items-start w-100">
-                                <h3 class="text-lg font-semibold leading-6 text-gray-800">Order Id: <?= $o["id_pesanan"]; ?> </h3>
-                                <span class="btn btn-success">Status</span>
-                            </div>
-                            <div class="d-flex flex-column mt-4">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <p class="text-sm leading-none text-gray-600">
-                                            <strong>Product:</strong> <span class="text-gray-800"><?= $matchingProduct['nama']; ?></span>
-
-                                        </p>
-                                        <p>
-                                            <strong>Quantity:</strong> <span class="text-gray-800"><?= $o["jumlah_produk"]; ?></span>
-                                        </p>
-                                        <p>
-                                            <strong>Total Price:</strong> <span class="text-gray-800">Rp <?= $o["total_harga"]; ?></span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="d-flex mt-4 justify-content-end align-items-center w-100">
-                                <a href="/orderHistory/<?= $o["id_pesanan"]; ?>" class="btn btn-primary">Details</a>
-                            </div>
-                        </div>
-                    </div>
-
-
-                <?php endforeach; ?>
-            </div>
+  <div class="row justify-content-center align-items-center mb-5">
+    <div class="col-lg-9">
+      <h2 class="fw-bold mb-3">Order History</h2>
+      <?php if (session()->getFlashdata('pesan')) : ?>
+        <div class="alert alert-success my-2" role="alert">
+          <?= session()->getFlashdata('pesan'); ?>
         </div>
+      <?php endif; ?>
+      <div class="d-flex justify-content-center align-items-center w-100 mt-2 flex-column space-y-4">
+        <?php foreach ($orderHistory as $o) : ?>
+          <?php
+          $produk_id = $o["produk_id"];
+          $matchingProduct = array_filter($produk, function ($produk) use ($produk_id) {
+            return $produk['id'] == $produk_id;
+          });
+          $matchingProduct = reset($matchingProduct);
+          ?>
+          <div class="d-flex flex-row justify-content-start align-items-start border border-gray-200 w-100 px-3 my-3 py-3">
+            <img src="/img/<?= $matchingProduct['gambar']; ?>" alt="<?= $matchingProduct['nama']; ?>" style="width: 20%; aspect-ratio: 1/1; object-fit: cover; object-position: center;">
+
+            <div class="d-flex flex-column justify-content-start align-items-start w-100 px-4">
+              <div class="d-flex justify-content-between align-items-start w-100">
+                <h3 class="text-lg font-semibold leading-6 text-gray-800">Order Id: <?= $o["id_pesanan"]; ?> </h3>
+                <span class="btn btn-success"><?= $o['status']; ?></span>
+              </div>
+              <div class="d-flex flex-column mt-4">
+                <div class="row">
+                  <div class="col-12">
+                    <p class="text-sm leading-none text-gray-600">
+                      <strong>Product:</strong> <span class="text-gray-800"><?= $matchingProduct['nama']; ?></span>
+
+                    </p>
+                    <p>
+                      <strong>Quantity:</strong> <span class="text-gray-800"><?= $o["jumlah_produk"]; ?></span>
+                    </p>
+                    <p>
+                      <strong>Total Price:</strong> <span class="text-gray-800">Rp <?= $o["total_harga"]; ?></span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="d-flex mt-4 justify-content-end align-items-center w-100">
+                <a href="/orderHistory/<?= $o["id_pesanan"]; ?>" class="btn btn-primary">Details</a>
+              </div>
+            </div>
+          </div>
+
+
+        <?php endforeach; ?>
+      </div>
     </div>
+  </div>
 </div>
 
 <?= $this->endSection(); ?>
